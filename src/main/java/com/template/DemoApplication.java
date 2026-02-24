3310c5ed0a8bd4ce589ed7811e8d6f6fb76fba89
Yisak Metaferiya <yisakmetaferiya1@gmail.com>
2026-02-24 23:58:01 +0400

    FEAT: update application properties, add JWT utility class, and configure MySQL datasource

--- src/main/java/com/template/DemoApplication.java
+++ src/main/java/com/template/DemoApplication.java
@@ -2,13 +2,17 @@ package com.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class DemoApplication {
-
 	 static void main(String[] args) {
          System.out.println("service is running on http://localhost:8080/");
+         BCryptPasswordEncoder bc = new  BCryptPasswordEncoder(10);
+         System.out.println(bc.encode("123456"));
          SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
+
+
